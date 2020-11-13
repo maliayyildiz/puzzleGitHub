cfca089e95114efdf3ae4fb7018b9062e356abfe
maliayyildiz <maliayyildiz61@gmail.com>
2020-11-13 22:31:56 +0100

    ikinci

--- src/test/java/Class.java
+++ src/test/java/Class.java
@@ -3,7 +3,7 @@ import sun.lwawt.macosx.CSystemTray;
 public class Class {
     public static void main(String[] args) {
         System.out.println("selam");
-
+        System.out.println("selam2");
 
 
     }
